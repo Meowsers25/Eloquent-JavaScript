09f668706e27fd28aba0742a80d5d1e8055be8ad
Chris <c.lamothe25@gmail.com>
2019-02-26 09:24:02 -0500

    More arrow functions

--- chapt03/arrowFunct.js
+++ chapt03/arrowFunct.js
@@ -6,3 +6,21 @@ const power = (base, exponent) => {
   return result 
 }
 console.log(power(2, 10))
+
+// const square = (x) => {return x * x}
+
+// when there is one parameter the parens can be eliminated
+// when there is on expression returned eliminate the brackets
+// this is the same as above
+
+const square = x => x * x
+console.log(square(5))
+
+// when no parameter use empty parens
+
+// const horn = () => {console.log('Toot')}
+
+// or..
+
+const horn = () => console.log('Toot')
+horn()
