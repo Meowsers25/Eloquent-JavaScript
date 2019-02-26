3422789563c622cc22a1f9ddbb72fbc8ea75c066
Chris <c.lamothe25@gmail.com>
2019-02-26 06:50:28 -0500

    Function declarations

---
+++ chapt03/functDeclaration.js
@@ -0,0 +1,7 @@
+console.log(`The future will have: ${future()}`)
+
+// function declations are not part of regular 
+// top to bottom flow. This code will work
+function future() {
+  return "Flying Cars"
+}
